2ca4d78c89755128960c671582fb2363c6d876f8
hjxxzk <273017@student.pwr.edu.pl>
2024-09-12 14:25:23 +0200

    Create classes and program structure

---
+++ src/main/java/org/example/Frame.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Frame {
+    final private String result;
+
+    public Frame(String result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/org/example/Line.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.List;
+
+public class Line {
+    private int points;
+    private List<Frame> frames;
+
+    public Line(String[] rolls) {
+        this.points = 0;
+
+    }
+
+}
